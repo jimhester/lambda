9c5593679f09d1c1c3fc47e2b265d9eadcbd87e8
Jim Hester <james.f.hester@gmail.com>
2015-09-19 12:51:30 -0400

    Support default arguments in .() expressions

--- R/package.R
+++ R/package.R
@@ -3,28 +3,45 @@
 #' Function to create anonymous functions (lambdas) in a very concise manner.
 #' Simply write the body of the function as you would normally.  If you want a
 #' given variable to appear as one of the arguments to the function, put it in
-#' \code{.()}.
+#' \code{.()}.  Default arguments can be specified as you normally would in the
+#' argument list.
 #' @param expr Expression that is the body of the function
 #' @param env the environment the new function will have
 #' @examples
 #' Reduce(f(.(x) + .(y)), 1:10)
 #'
-#' compact1 <- f(FilterNegate(is.null(.(x))))
+#' compact1 <- f(Filter(Negate(is.null(.(x)))))
 #'
 #' f(runif(n = rpois(1, 5), .(...)))
 #' @export
 f <- function(expr, env = parent.frame()) {
-  args <- NULL
+
+  if (missing(expr)) {
+    stop("argument ", sQuote("expr"), " is missing, with no default", call. = FALSE)
+  }
+
+  args <- list()
   recurse <- function(e) {
     if (length(e) <= 1) e
     else if (e[[1]] == as.name(".")) {
-      args <<- append(args, e[[2]])
-    e[[2]]
+      e <- e[-1]
+      args <<- append(args, as.list(e))
+      if (!is.null(names(e))) {
+        as.symbol(names(e))
+      } else {
+        e[[1]]
+      }
     } else {
       as.call(lapply(e, recurse))
     }
   }
   body <- recurse(substitute(expr))
 
-  eval(call("function", as.pairlist(setNames(lapply(args, function(x) quote(expr=)), args)), body), env)
+  if (is.null(names(args))) names(args) <- rep.int("", length(args))
+
+  unnamed <- names(args) == ""
+  names(args)[unnamed] <- args[unnamed]
+  args[unnamed] <- list(quote(expr=))
+
+  eval(call("function", as.pairlist(args), body), env)
 }
